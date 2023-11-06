d88713bf10a0a2c96579f6d965dfd755a81bca64
TristanAly <tristan.aly@gmail.com>
2023-11-06 14:37:24 +0100

    add banner and design

--- ArgentBank-website/src/pages/Home/Home.jsx
+++ ArgentBank-website/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../../components/Banner/Banner";
 import Header from "../../components/Header/Header";
 
 export const Home = () => {
   return (
     <div>
       <Header />
+      <Banner />
     </div>
   );
 };
