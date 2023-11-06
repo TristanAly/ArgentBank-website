1474a433952c7cd7eb11a18b2f7fa30f7f0f093c
TristanAly <tristan.aly@gmail.com>
2023-11-06 16:24:29 +0100

    correction signin form

--- ArgentBank-website/src/components/Form/SignIn.jsx
+++ ArgentBank-website/src/components/Form/SignIn.jsx
@@ -3,24 +3,26 @@ import { FaRegUserCircle } from "react-icons/fa";
 
 const SignIn = () => {
   return (
-    <main class="main bg-dark">
-      <section class="sign-in-content">
+    <main className="main bg-dark">
+      <section className="sign-in-content">
         <FaRegUserCircle className="iconUserCircle" />
         <h2>Sign In</h2>
         <form>
-          <div class="input-wrapper">
-            <label for="username">Username</label>
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
             <input type="text" id="username" />
           </div>
-          <div class="input-wrapper">
-            <label for="password">Password</label>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
             <input type="password" id="password" />
           </div>
-          <div class="input-remember">
+          <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+            <label htmlFor="remember-me">Remember me</label>
           </div>
-          <button class="sign-in-button">Sign In</button>
+          <button type="submit" className="sign-in-button">
+            Sign In
+          </button>
         </form>
       </section>
     </main>
